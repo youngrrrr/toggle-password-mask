945b8f06c3be6dcb5129defc5d0e9e9899474f49
youngrrrr <young-rae_kim@brown.edu>
2015-11-23 22:19:52 -0500

    added comments and size safeguard

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,28 +1,58 @@
-var toggleIcon = $(".toggle-icon");
-var passwordField = $("#password-field")[0];
+/* Requires jQuery v2.1.4 */
 
+/* Initialize global variables */
+var toggleIcon = $(".toggle-icon");
+var passwordField = $(".toggle-password");
+
+/* Makes sure icon size is consistent with respect
+   to input password field */
+var toggleIconSize = passwordField.height();
 toggleIcon
+	.height(toggleIconSize)
+	.width(toggleIconSize);
+
+/* Hide/show password logic */
+toggleIcon
+	/* disables dragging of image 
+	   (mostly aesthetic) */
 	.on("dragstart", function(event) {
 		event.preventDefault();
 	})
+	/* if a user moves their mouse,
+	   hide password again */
 	.mousemove(function() {
 		hidePassword();
 	})
+	/* user must click and hold to 
+	   show password */
 	.mousedown(function() {
 		showPassword();
 	})
+	/* if a user releases mouse,
+	   hide password again */
 	.mouseup(function() {
 		hidePassword();
 	});
 
+/*  */
 function showPassword() {
-	toggleIcon.attr("src", "assets/images/show.png");
-	toggleIcon.attr("alt", "SHOW");
-	passwordField.type = "text";
+	/* Change image properties:
+	   (1) replaces image to seeing eye
+	   (2) changes alternate text value */
+	toggleIcon.attr("src", "assets/images/show.png"); //(1)
+	toggleIcon.attr("alt", "SHOW"); //(2)
+
+	/* Change input field to type text to show password. */
+	passwordField[0].type = "text"; //show password
 }
 
 function hidePassword() {
-	toggleIcon.attr("src", "assets/images/hide.png");
-	toggleIcon.attr("alt", "HIDE");
-	passwordField.type = "password";
+	/* Change image properties:
+	   (1) replaces image to crossed out eye
+	   (2) changes alternate text value */
+	toggleIcon.attr("src", "assets/images/hide.png"); //(1)
+	toggleIcon.attr("alt", "HIDE"); //(2)
+
+	/* Change input field to type password to hide it again. */
+	passwordField[0].type = "password";
 }
